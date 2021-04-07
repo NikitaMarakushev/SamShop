2214850822026fb46943e4e5f0adc8bd613a09b4
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-04-07 19:51:34 +0300

    jwt dev

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,24 @@
 const ApiError = require('../error/ApiError');
-const { noExtendLeft } = require("sequelize/types/lib/operators");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {User, Basket} = require('../models/models');
 
 class UserController {
-    async registartion(req, res) {
+    async registartion(req, res, next) {
+        const {email, password, role} = req.body;
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некорректный email или пароль'));
+        }
+        const candidate = await User.findOne({where: {email}});
+        if (candidate) {
+            return next(ApiError.badRequest('Полльзователь уже существует'));
+        }
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({email, role, password: hashPassword});
+        const basket = await Basket.create({userId: user.id});
+        const jwt = jwt.sign({id: user.id, email: email, role}, process.env.SECRET_KEY, {expiresIn: '24h'});
 
+        return res.json({token});
     }
 
     async login(req, res) {
