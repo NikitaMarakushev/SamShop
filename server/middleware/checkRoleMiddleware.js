2d72a194fd010f92df92a9083ca6d1f49466edea
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-04-08 07:13:09 +0300

    more middleware

---
+++ server/middleware/checkRoleMiddleware.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const { fn } = require('sequelize/types');
+
+module.exports =  function(role) {
+    return function (req, res, next) {
+        if (req.method === "OPTIONS") {
+            next();
+        }
+        try {
+            const token = req.header.authorization.split(' ')[1];
+    
+            if (!token) {
+                res.status(401).json({message: "Пользователь не авторизован"});
+            }
+            const decoded = jwt.verify(token, process.env.SECRET_KEY);
+            if (decoded.role !== role) {
+                return res.status(403).json({message: "Нет доступа"});
+            }
+            req.user = decoded;
+            next();
+        } catch (e) {
+            res.status(401).json({message: "Пользователь не авторизован"});
+        }
+    } 
+}
+
+
+
+
+    
+
+
+
