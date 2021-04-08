ca6c55b5fd4d3d000423f45f1e2fea9fc8e9d4f8
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-04-08 15:08:31 +0300

    prefull server-side

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,9 @@
 const Router = require('express');
 const router = new Router();
+const deviceRouter = require('./deviceRouter');
+const userRouter = require('./userRouter');
+const brandRouter = require('./brandRouter');
+const typeRouter = require('./typeRouter');
 
 router.use('/user');
 router.use('/type');
--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,13 +1,9 @@
 const Router = require('express');
 const router = new Router();
-const deviceRouter = require('./deviceRouter');
-const userRouter = require('./userRouter');
-const brandRouter = require('./brandRouter');
-const typeRouter = require('./typeRouter');
+const typeController = require('../controllers/typeController');
+const checkRole = require('../middleware/checkRoleMiddleware');
 
-router.use('/user', userRouter);
-router.use('/type', typeRouter);
-router.use('/brand', brandRouter);
-router.use('/device', deviceRouter);
+router.post('/', checkRole('ADMIN', typeController.create));
+router.get('/', typeController.getAll);
 
 module.exports = router;
