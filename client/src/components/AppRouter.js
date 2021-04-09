6c2a86668bed84fd0957f5c1675ee9856d321d57
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-04-09 11:45:44 +0300

    added context

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import {authRoutes, publicRoutes} from "../routes";
 import {SHOP_ROUTE} from "../utils/consts";
+import {Context} from "../index";
 
 const AppRouter = () => {
-    const isAuth = false;
-
+    const {user} = useContext(Context);
     return (
         <Switch>
             {isAuth && authRoutes.map(({path, Component}) =>
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createContext} from "react";
+import UserStore from "./store/UserStore";
+
+export const Context = createContext(null);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Context.Provider value={{
+        user: new UserStore()
+    }}>
+        <App/>
+    </Context.Provider>,
   document.getElementById('root')
 );
-
-reportWebVitals();
