26a842fa9264b63b33f0af7b525fa2c8be03967c
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-05-04 14:47:32 +0300

    Changing logic of AdminPanel and filing the Create components with code

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Modal from "react-bootstrap/Modal";
+import Button, { Form } from "react-bootstrap";
 
-const CreateDevice = () => {
+const CreateDevice = ({show, onHide}) => {
     return (
-        <div>
-            
-        </div>
+        <Modal size="lg" centered show={show} onHide={onHide}>
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Добавить тип
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Control 
+                        placeholder={"Введите название типа"}
+                    />
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="outline-danger" onClick={props.onHide}>Закрыть</Button>
+                <Button variant="outline-success" onClick={props.onHide}>Добавить</Button>
+            </Modal.Footer>
+        </Modal>
     )
 }
 
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import {Button, Container} from "react-router-dom";
+import CreateBrand from "../components/modal/CreateBrand";
+import CreateDevice from "../components/modal/CreateDevice";
+import CreateType from "../components/modal/CreateType";
 
 const Admin = () => {
+    const [brandVisible, setBrandVisible] =  useState(false); 
+    const [typeVisible, setTypeVisible] =  useState(false); 
+    const [deviceVisible, setDeviceVisible] =  useState(false); 
+
     return (
         <Container className="d-flex flex-column">
             <Button variant={"outline-dark"} className="mt-4 p-2"> 
